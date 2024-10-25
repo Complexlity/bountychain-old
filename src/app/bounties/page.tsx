3775eb24fc1b3b2739e018612f1eed0717e674fa
Complexlity <nwalozielijah@gmail.com>
2024-10-25 17:46:20 +0100

    chore: remove floating button component

--- src/app/bounties/page.tsx
+++ src/app/bounties/page.tsx
@@ -15,7 +15,6 @@ import {
 import { BountyCard } from "@/features/bounties/components/bounty-card";
 import { CreateBountyDialog } from "@/features/bounties/components/create-bounty-dialog";
 import { useBounties } from "@/features/bounties/hooks/bounties";
-import FloatingButton from "@/features/bounties/components/floating-button";
 
 export default function BountiesPage() {
   const { data, isPending } = useBounties();
@@ -46,6 +45,7 @@ export default function BountiesPage() {
         {isPending ? (
           <div className="flex items-center justify-center">
             <Plus className="animate-spin h-5 w-5" />
+            Loading...
           </div>
         ) : filteredBounties && filteredBounties.length > 0 ? (
           <div className="grid justify-center gap-6 py-2 md:grid-cols-2 lg:grid-cols-3 grid-flow-row">
@@ -57,7 +57,20 @@ export default function BountiesPage() {
           <div>There are no bounties</div>
         )}
         <CreateBountyDialog>
-          <FloatingButton />
+          <button
+            className="group fixed bottom-10 right-10 flex cursor-pointer items-end justify-end p-2"
+            type="button"
+          >
+            {/* main */}
+            <span className="absolute z-50 flex items-center justify-center rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white shadow-xl">
+              <Plus className="duration-[0.6s] h-6 w-6 transition-all group-hover:rotate-90" />
+            </span>
+
+            {/* sub middle */}
+            <span className="duration-[0.2s] absolute flex w-40 scale-x-0 justify-center rounded-xl bg-zinc-200 py-2 transition-all ease-out group-hover:-translate-x-8 group-hover:-translate-y-12 group-hover:scale-x-100">
+              Post New Bounty
+            </span>
+          </button>
         </CreateBountyDialog>
       </MaxWidthWrapper>
     </>
