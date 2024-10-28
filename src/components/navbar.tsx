406e10c3144f394dfb96c3cfeb622d56dc63bc9f
Complexlity <nwalozielijah@gmail.com>
2024-10-28 11:40:14 +0100

    chore: responsive nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,16 +9,12 @@ export default function Navbar() {
     <div>
       <header className="flex h-14 items-center justify-between px-4 py-8 lg:px-6">
         <Link className="flex items-center justify-center" href="/">
-          <Coins className="h-6 w-6" />
-          <span className="ml-2 text-lg font-bold">BountyChain</span>
+          <Coins className="h-8 w-8" />
+          <span className="ml-2 text-xl font-bold hidden sm:inline-flex">
+            BountyChain
+          </span>
         </Link>
         <nav className="flex items-center gap-4 sm:gap-6">
-          {/* <Link
-						className="text-sm font-medium hover:underline underline-offset-4"
-						to="#"
-					>
-						Features
-					</Link> */}
           <Link
             className="text-sm font-medium underline-offset-4 hover:underline"
             href="/"
@@ -32,8 +28,9 @@ export default function Navbar() {
             Bounties
           </Link>
           <CreateBountyDialog>
-            <Button className="bold hidden h-fit rounded-none md:inline-block">
-              Create New Bounty
+            <Button className="bold h-fit rounded-none md:inline-block px-2 py-2 text-sm">
+              <span className="hidden sm:inline">Create</span>{" "}
+              <span>New Bounty</span>
             </Button>
           </CreateBountyDialog>
           <ConnectButton chainStatus={"icon"} showBalance={false} />
