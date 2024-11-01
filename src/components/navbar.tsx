0b9d835039d7b6e0b30c1297404c8935c7a86557
Complexlity <nwalozielijah@gmail.com>
2024-11-01 15:11:50 +0100

    feat: indicate route on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Coins } from "lucide-react";
 import { Button } from "./ui/button";
 import { CreateBountyDialog } from "@/features/bounties/components/create-bounty-dialog";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <div>
       <header className="flex h-14 items-center justify-between px-4 py-8 lg:px-6">
@@ -16,20 +22,30 @@ export default function Navbar() {
         </Link>
         <nav className="flex items-center gap-4 sm:gap-6">
           <Link
-            className="text-sm font-medium underline-offset-4 hover:underline"
+            className={cn(
+              "text-sm sm:text-base md:text-xl font-medium underline-offset-4 transition-colors",
+              pathname === "/"
+                ? "text-primary font-semibold underline"
+                : "text-muted-foreground hover:text-primary hover:underline"
+            )}
             href="/"
           >
             Home
           </Link>
           <Link
-            className="text-sm font-medium underline-offset-4 hover:underline"
+            className={cn(
+              "text-sm sm:text-base md:text-xl font-medium underline-offset-4 transition-colors",
+              pathname === "/bounties"
+                ? "text-primary font-semibold underline"
+                : "text-muted-foreground hover:text-primary hover:underline"
+            )}
             href="/bounties"
           >
             Bounties
           </Link>
           <CreateBountyDialog>
-            <Button className="bold h-fit rounded-none md:inline-block px-2 py-2 text-sm">
-              <span className="hidden sm:inline">Create</span>{" "}
+            <Button className="bold flex gap-1 h-fit rounded-none md:inline-block px-2 py-2 text-sm">
+              <span className="hidden sm:inline">Create</span>
               <span>New Bounty</span>
             </Button>
           </CreateBountyDialog>
