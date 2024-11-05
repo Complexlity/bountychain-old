fada9c6e81f039c533bba54f7cc2145082b67654
Complexlity <nwalozielijah@gmail.com>
2024-11-05 09:31:18 +0100

    fix: ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ function OngoingBounties() {
             <p>Loading featured bounties...</p>
           </div>
         )}
-        <div className="mx-auto grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3  justify-center">
           {ongoingBounties.length > 0 &&
             ongoingBounties.map((bounty) => (
               <BountyCard bounty={bounty} key={bounty.id} />
