1e22125ae97a46d7dbc80a4b309cc3d3fa493faf
Complexlity <nwalozielijah@gmail.com>
2024-11-15 15:45:45 +0100

    fix: nav button spacing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
             </div>
 
             <CreateBountyDialog>
-              <Button className="bold flex gap-1 h-fit rounded-none md:inline-block px-2 py-2 text-sm">
+              <Button className="bold flex h-fit rounded-none gap-1 px-2 py-2 text-sm">
                 <span className="hidden sm:inline">Create</span>
                 <span>New Bounty</span>
               </Button>
