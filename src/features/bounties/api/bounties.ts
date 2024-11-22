9387b9486aacc52dcd2e65952c5d2841ad26ed00
Complexlity <nwalozielijah@gmail.com>
2024-11-22 13:45:44 +0100

    fix: user can send incorrect amount to db

--- src/features/bounties/api/bounties.ts
+++ src/features/bounties/api/bounties.ts
@@ -2,7 +2,7 @@ import { insertBountiesSchema } from "@/db/schema";
 import serverEnv from "@/lib/server-env";
 import { getPublicClient, supportedChains } from "@/lib/viem";
 import { NextRequest } from "next/server";
-import { Address } from "viem";
+import { Address, formatUnits } from "viem";
 import { createBounty, getBounties } from "../lib/queries";
 import { CreateBountySchema } from "../lib/types";
 import { isZeroAddress } from "../lib/utils";
@@ -18,14 +18,16 @@ export async function post({ request }: { request: NextRequest }) {
   if (!success) {
     return { message: "Invalid data", statusCode: 422 };
   }
-  console.log("I was parsed properly...");
   const activeChain = serverEnv.NEXT_PUBLIC_ACTIVE_CHAIN;
   const publicClient = getPublicClient(activeChain);
   const type =
     (data.token as keyof (typeof supportedChains)[typeof activeChain]["contracts"]) ??
     "eth";
-  const { address: contractAddress, abi: bountyAbi } =
-    supportedChains[activeChain].contracts[type];
+  const {
+    address: contractAddress,
+    abi: bountyAbi,
+    decimals,
+  } = supportedChains[activeChain].contracts[type];
   const bountyDetails = await publicClient.readContract({
     address: contractAddress,
     abi: bountyAbi,
@@ -35,9 +37,15 @@ export async function post({ request }: { request: NextRequest }) {
   if (!bountyDetails || isZeroAddress(bountyDetails[0])) {
     return { message: "Bounty not found", statusCode: 400 };
   }
-  console.log("Bounty found. Creating on db...");
-  const newBounty = await createBounty(data).catch((e) => {
-    console.log(e);
+
+  //Use the amount from the contract rather than from the request. It doesn't affect payment but it could show incorrectly on the UI
+  const bountyAmount = bountyDetails[1];
+  const readableAmount = formatUnits(bountyAmount, decimals);
+
+  const newBounty = await createBounty({
+    ...data,
+    amount: Number(readableAmount),
+  }).catch((e) => {
     if (e.code === "SQLITE_CONSTRAINT_UNIQUE") {
       return { message: "Bounty already exists", statusCode: 400 };
     }
