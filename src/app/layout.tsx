b1a79b21ea18b67c0e916649eed072eac4530877
Complexlity <nwalozielijah@gmail.com>
2024-11-23 12:15:57 +0100

    chore: move font folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
+import { Footer } from "@/components/footer";
+import Navbar from "@/components/navbar";
+import Providers from "@/components/providers";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Providers from "@/components/providers";
-import Navbar from "@/components/navbar";
-import "@rainbow-me/rainbowkit/styles.css";
-import { Footer } from "@/components/footer";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
